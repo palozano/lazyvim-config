f8cd4aaca75acc20049a01804f95be597f795652
Pablo Lozano <pablo@t3rn.io>
2023-07-04 11:33:56 +0200

    feat: oil as a better file explorer

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,9 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- better exits
 vim.keymap.set("i", "jk", "<ESC>", { noremap = true, silent = true, desc = "jk as <ESC> key" })
-
 vim.keymap.set("t", "<ESC>", "<C-\\><C-n>", { noremap = true, silent = true, desc = "Exit terminal mode" })
+
+-- open oil with hyphen
+vim.keymap.set("n", "-", require("oil").open_float, { desc = "Open parent directory" })
